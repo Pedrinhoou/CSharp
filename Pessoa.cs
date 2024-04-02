95bc6e9ae2814658bc9d0107d6b3330cf9c048f2
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-04-02 16:30:24 -0400

    add classe estudante

--- Pessoa.cs
+++ Pessoa.cs
@@ -22,3 +22,17 @@ public class Pessoa{
         set{cpf=value;}
     }
 }    
+public class Estudante : Pessoa{
+    private String curso;
+
+    // Construtor da classe Estudante
+    public Estudante(string nome, int idade, string cpf, string curso) ; base(nome, idade, cpf){
+        Curso = curso;
+    }
+
+    //Propriedades da classe Estudante que permite acesso controlado a um campo privado
+    public string Curso{
+        get { return curso;}
+        set {curso = value;}
+    }
+}
